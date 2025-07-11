7b4b582f95d575789f86832baa76b3e0595b9acc
Marc Stober <marcstober@gmail.com>
2025-07-11 16:02:00 -0400

    fix typos

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((400, 300))
 clock = pygame.time.Clock()
 running = True
 
-# -- Initialize other global variables here --
+# -- initialize other global variables here --
 
 while running:
     # fill window with a background color
@@ -17,7 +17,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # -- put the rest of you code here --
+    # -- put the rest of your code here --
 
     pygame.display.flip()
     clock.tick(60)
